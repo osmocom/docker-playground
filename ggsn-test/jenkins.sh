d95cdf50688e637ba9d45fcdca9dd00b2b6b79c4
Harald Welte <laforge@gnumonks.org>
2017-10-15 17:09:27 +0000

    ggns-test/jenkins.sh: Remove need for 'helper' container
    Instaed of using docker volumes, mount a /tmp directory into the
    containers, which makes copying the resulting files *way* easier.

--- ggsn-test/jenkins.sh
+++ ggsn-test/jenkins.sh
@@ -10,36 +10,34 @@ if [ "x$WORKSPACE" = "x" ]; then
 	WORKSPACE=/tmp
 fi
 
+VOL_BASE_DIR=`mktemp -d`
+mkdir $VOL_BASE_DIR/ggsn-tester
+cp GGSN_Tests.cfg $VOL_BASE_DIR/ggsn-tester/
+
+mkdir $VOL_BASE_DIR/ggsn
+cp ../osmo-ggsn-master/osmo-ggsn.cfg $VOL_BASE_DIR/ggsn/
+
 # start container with ggsn in background
-docker volume rm ggsn-vol
 docker run	--cap-add=NET_ADMIN \
 		--device /dev/net/tun:/dev/net/tun \
 		--sysctl net.ipv6.conf.all.disable_ipv6=0 \
 		--rm \
 		--network sigtran --ip 172.18.0.201 \
-		-v ggsn-vol:/data \
+		-v $VOL_BASE_DIR/ggsn:/data \
 		--name ggsn -d \
 		$REPO_USER/osmo-ggsn-master
 
 # start docker container with testsuite in foreground
-docker volume rm ggsn-test-vol
 docker run	--rm \
 		--sysctl net.ipv6.conf.all.disable_ipv6=0 \
 		--network sigtran --ip 172.18.0.202 \
-		-v ggsn-test-vol:/data \
+		-v $VOL_BASE_DIR/ggsn-tester:/data \
 		$REPO_USER/ggsn-test
 
 # stop GGSN after test has completed
 docker container stop ggsn
 
-# start some stupid helper container so we can access the volume
-docker run	--rm \
-		-v ggsn-test-vol:/ggsn-tester \
-		-v ggsn-vol:/ggsn \
-		--name helper -d \
-		busybox /bin/sh -c 'sleep 1000 & wait'
 rm -rf $WORKSPACE/logs
 mkdir -p $WORKSPACE/logs
-docker cp helper:/ggsn-tester $WORKSPACE/logs
-docker cp helper:/ggsn $WORKSPACE/logs
-docker container stop -t 0 helper
+cp -a $VOL_BASE_DIR/* $WORKSPACE/logs/
+#rm -rf $VOL_BASE_DIR
