a59c18da015f4f341e92be312e6762fd1347f537
Harald Welte <laforge@gnumonks.org>
2018-03-01 15:09:36 +0000

    Add ttcn3-hlr-test container

---
+++ ttcn3-hlr-test/jenkins.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+. ../jenkins-common.sh
+
+network_create 172.18.10.0/24
+
+mkdir $VOL_BASE_DIR/hlr-tester
+cp HLR_Tests.cfg $VOL_BASE_DIR/hlr-tester/
+
+mkdir $VOL_BASE_DIR/hlr
+cp osmo-hlr.cfg $VOL_BASE_DIR/hlr/
+
+echo Starting container with HLR
+docker run	--rm \
+		--network $NET_NAME --ip 172.18.10.20 \
+		-v $VOL_BASE_DIR/hlr:/data \
+		--name ${BUILD_TAG}-hlr -d \
+		$REPO_USER/osmo-hlr-master \
+		/usr/local/bin/osmo-hlr
+
+echo Starting container with HLR testsuite
+docker run	--rm \
+		--network $NET_NAME --ip 172.18.10.103 \
+		-e "TTCN3_PCAP_PATH=/data" \
+		-v $VOL_BASE_DIR/hlr-tester:/data \
+		--name ${BUILD_TAG}-ttcn3-hlr-test \
+		$REPO_USER/ttcn3-hlr-test
+
+echo Stopping containers
+docker container kill ${BUILD_TAG}-hlr
+
+network_remove
+
+rm -rf $WORKSPACE/logs
+mkdir -p $WORKSPACE/logs
+cp -a $VOL_BASE_DIR/* $WORKSPACE/logs/
+cat $WORKSPACE/logs/hlr-tester/junit-*.log || true
