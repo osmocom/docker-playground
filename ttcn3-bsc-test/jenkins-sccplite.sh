060d47f07192c71b3e95def8a4697b6f3dc899bd
Harald Welte <laforge@gnumonks.org>
2018-08-24 08:31:12 +0200

    use non-overlapping IP networks for bsc-sccplite and SIP tests
    The osmo-sip-connector test is already using 172.18.11.x, let's migrate
    sccplite to 172.18.12.x to prevent any conflicts when running both tests
    on the same build slave (docker instance).
    
    Change-Id: I96beed1f5e01bdb8e77e20f77a459aa006de10fe

--- ttcn3-bsc-test/jenkins-sccplite.sh
+++ ttcn3-bsc-test/jenkins-sccplite.sh
@@ -8,11 +8,11 @@ cp sccplite/BSC_Tests.cfg $VOL_BASE_DIR/bsc-tester/
 mkdir $VOL_BASE_DIR/bsc
 cp sccplite/osmo-bsc.cfg $VOL_BASE_DIR/bsc/
 
-network_create 172.18.11.0/24
+network_create 172.18.12.0/24
 
 echo Starting container with BSC
 docker run	--rm \
-		--network $NET_NAME --ip 172.18.11.20 \
+		--network $NET_NAME --ip 172.18.12.20 \
 		-v $VOL_BASE_DIR/bsc:/data \
 		--name ${BUILD_TAG}-bsc -d \
 		$REPO_USER/osmo-bsc-master
@@ -20,14 +20,14 @@ docker run	--rm \
 for i in `seq 0 2`; do
 	echo Starting container with OML for BTS$i
 	docker run	--rm \
-			--network $NET_NAME --ip 172.18.11.10$i \
+			--network $NET_NAME --ip 172.18.12.10$i \
 			--name ${BUILD_TAG}-bts$i -d \
-			$REPO_USER/osmo-bts-master /usr/local/bin/respawn.sh osmo-bts-omldummy 172.18.11.20 $((i + 1234)) 1
+			$REPO_USER/osmo-bts-master /usr/local/bin/respawn.sh osmo-bts-omldummy 172.18.12.20 $((i + 1234)) 1
 done
 
 echo Starting container with BSC testsuite
 docker run	--rm \
-		--network $NET_NAME --ip 172.18.11.203 \
+		--network $NET_NAME --ip 172.18.12.203 \
 		-e "TTCN3_PCAP_PATH=/data" \
 		-v $VOL_BASE_DIR/bsc-tester:/data \
 		--name ${BUILD_TAG}-ttcn3-bsc-test \
