9b8a6f74c7ee4fe750f33e5c55eacef1acb798fd
Daniel Willmann <dwillmann@sysmocom.de>
2018-09-18 19:35:10 +0200

    osmo-cn-latest: Make sure sgsn/ggsn work and are reachable from outside
    Change-Id: Ibbfa6f3cd0a15566675a12653421e5b4523cecfe

--- osmo-cn-latest/run.sh
+++ osmo-cn-latest/run.sh
@@ -2,7 +2,7 @@
 
 . ../jenkins-common.sh
 
-network_create 192.168.42.0/24
+docker network create --subnet 192.168.42.0/24 $NET_NAME
 
 mkdir $VOL_BASE_DIR/osmo-cn
 cp osmo-stp.cfg $VOL_BASE_DIR/osmo-cn/
@@ -15,7 +15,10 @@ cp hlr.db $VOL_BASE_DIR/osmo-cn/
 
 echo Starting Osmocom core services
 docker run	--rm \
+		--cap-add=NET_ADMIN \
+		--device /dev/net/tun:/dev/net/tun \
 	    	--network $NET_NAME --ip 192.168.42.10 \
+		-p 0.0.0.0:23000:23000/udp \
 		-v $VOL_BASE_DIR/osmo-cn:/data \
 	       	--name osmo-cn \
 	       	$REPO_USER/osmo-cn-latest
