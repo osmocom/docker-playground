96a0c5f73afce09eca1a1407ecd7799b3fd2634b
Oliver Smith <osmith@sysmocom.de>
2018-10-12 16:36:08 +0200

    rename m3ua-test, sua-test to nplab-*-test
    Allows writing a generic Jenkins Job Builder config by renaming the
    container folders to match the job names. This needs changes in the
    Jenkins jobs, as done in Ie433925ee81a61c5788b4a6f2bc5b89c2689d251.
    
    Related: OS#3268
    Change-Id: Iedf9a8cd9af1da674e018a08a977490520e602de

--- nplab-sua-test/jenkins.sh
+++ nplab-sua-test/jenkins.sh
@@ -6,30 +6,31 @@ docker_images_require \
 	"debian-jessie-build" \
 	"osmo-stp-$IMAGE_SUFFIX" \
 	"debian-stretch-titan" \
-	"m3ua-test"
+	"sigtran-tests" \
+	"nplab-sua-test"
 
-mkdir $VOL_BASE_DIR/m3ua-tester
-cp m3ua-param-testtool.scm all-sgp-tests.txt $VOL_BASE_DIR/m3ua-tester/
+mkdir $VOL_BASE_DIR/sua-tester
+cp sua-param-testtool-sgp.scm some-sua-sgp-tests.txt $VOL_BASE_DIR/sua-tester/
 
 mkdir $VOL_BASE_DIR/stp
 cp osmo-stp.cfg $VOL_BASE_DIR/stp/
 
-network_create 172.18.7.0/24
+network_create 172.18.6.0/24
 
 # start container with STP in background
 docker run	--rm \
 		--sysctl net.ipv6.conf.all.disable_ipv6=0 \
-		--network $NET_NAME --ip 172.18.7.200 \
+		--network $NET_NAME --ip 172.18.6.200 \
 		-v $VOL_BASE_DIR/stp:/data \
 		--name ${BUILD_TAG}-stp \
 		-d $REPO_USER/osmo-stp-$IMAGE_SUFFIX
 
 # start docker container with tests
 docker run	--rm \
-		--network $NET_NAME --ip 172.18.7.2 \
-		-v $VOL_BASE_DIR/m3ua-tester:/data \
-		--name ${BUILD_TAG}-m3ua-test \
-		$REPO_USER/m3ua-test > $WORKSPACE/logs/junit-xml-m3ua.log
+		--network $NET_NAME --ip 172.18.6.3 \
+		-v $VOL_BASE_DIR/sua-tester:/data \
+		--name ${BUILD_TAG}-sua-test \
+		$REPO_USER/nplab-sua-test > $VOL_BASE_DIR/junit-xml-sua.log
 
 docker container stop -t 1 ${BUILD_TAG}-stp
 
