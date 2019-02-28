c05330e8b4c50040d244f2610bcf88ead65d679e
Harald Welte <laforge@gnumonks.org>
2019-02-28 21:14:54 +0100

    PCU: Differentiate between tests with classic and SNS Gb dialects
    OsmoPCU needs to have the gb-dialect parameter configured right from
    startup.  This means we'll split the ttcn3-pcu-test into two suites,
    similarly to how we deal with AoIP / SCCPlite in the ttcn3-bsc-test.
    
    Change-Id: I6e844f9ac4b87e801661fb1a1bdd25a15148f474

---
+++ ttcn3-pcu-test-sns/jenkins.sh
@@ -0,0 +1,1 @@
+../ttcn3-pcu-test/jenkins-sns.sh
---
+++ ttcn3-pcu-test/jenkins-sns.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+. ../jenkins-common.sh
+IMAGE_SUFFIX="${IMAGE_SUFFIX:-master}"
+docker_images_require \
+	"debian-jessie-build" \
+	"osmo-pcu-$IMAGE_SUFFIX" \
+	"debian-stretch-titan" \
+	"ttcn3-pcu-test"
+
+network_create 172.18.14.0/24
+
+mkdir $VOL_BASE_DIR/pcu-tester
+mkdir $VOL_BASE_DIR/pcu-tester/unix
+cp sns/PCU_Tests.cfg $VOL_BASE_DIR/pcu-tester/
+
+mkdir $VOL_BASE_DIR/pcu
+mkdir $VOL_BASE_DIR/pcu/unix
+cp sns/osmo-pcu.cfg $VOL_BASE_DIR/pcu/
+
+mkdir $VOL_BASE_DIR/unix
+
+echo Starting container with PCU
+docker run	--rm \
+		--network $NET_NAME --ip 172.18.14.101 \
+		-v $VOL_BASE_DIR/pcu:/data \
+		-v $VOL_BASE_DIR/unix:/data/unix \
+		--name ${BUILD_TAG}-pcu-sns -d \
+		$REPO_USER/osmo-pcu-$IMAGE_SUFFIX \
+		/usr/local/bin/respawn.sh osmo-pcu -c /data/osmo-pcu.cfg
+
+echo Starting container with PCU testsuite
+docker run	--rm \
+		--network $NET_NAME --ip 172.18.14.10 \
+		-e "TTCN3_PCAP_PATH=/data" \
+		-v $VOL_BASE_DIR/pcu-tester:/data \
+		-v $VOL_BASE_DIR/unix:/data/unix \
+		--name ${BUILD_TAG}-ttcn3-pcu-test-sns \
+		$REPO_USER/ttcn3-pcu-test
+
+echo Stopping containers
+docker container kill ${BUILD_TAG}-pcu-sns
+
+network_remove
+rm -rf $VOL_BASE_DIR/unix
+collect_logs
