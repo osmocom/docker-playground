317aba0226a2eb747b462c911fde09f870e87a43
Harald Welte <laforge@gnumonks.org>
2019-05-23 16:00:26 +0200

    ttcn3-bscnat-test: Use unique sub-network 172.18.15.0/24
    For some reason, the bscnat tests were using the same subnet as the
    sip-connector tests, causing address clashes when trying to execute
    both test suites simultaneously:
    
    + echo Creating network ttcn3-sip-test
    Creating network ttcn3-sip-test
    + docker network create --internal --subnet 172.18.11.0/24 ttcn3-sip-test
    Error response from daemon: Pool overlaps with other one on this address space
    
    Change-Id: I62b01b4b291ed9d729139fcf9b0102459e7f5a6a

--- ttcn3-bscnat-test/jenkins.sh
+++ ttcn3-bscnat-test/jenkins.sh
@@ -16,18 +16,18 @@ mkdir $VOL_BASE_DIR/bscnat
 cp osmo-bsc-nat.cfg $VOL_BASE_DIR/bscnat/
 cp bscs.config $VOL_BASE_DIR/bscnat/
 
-network_create 172.18.11.0/24
+network_create 172.18.15.0/24
 
 echo Starting container with BSCNAT
 docker run	--rm \
-		--network $NET_NAME --ip 172.18.11.20 \
+		--network $NET_NAME --ip 172.18.15.20 \
 		-v $VOL_BASE_DIR/bscnat:/data \
 		--name ${BUILD_TAG}-bscnat -d \
 		$REPO_USER/osmo-nitb-$IMAGE_SUFFIX osmo-bsc_nat -c /data/osmo-bsc-nat.cfg
 
 echo Starting container with BSCNAT testsuite
 docker run	--rm \
-		--network $NET_NAME --ip 172.18.11.203 \
+		--network $NET_NAME --ip 172.18.15.203 \
 		-e "TTCN3_PCAP_PATH=/data" \
 		-v $VOL_BASE_DIR/bscnat-tester:/data \
 		--name ${BUILD_TAG}-ttcn3-bscnat-test \
