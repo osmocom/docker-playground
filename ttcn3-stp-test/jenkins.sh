5e70a2972774ce891aa302ec2f464b4e2840f094
Pau Espin Pedrol <pespin@sysmocom.de>
2019-10-24 16:45:59 +0200

    Introduce ttcn3-stp-test
    Currently builds from osmo-ttcn3-hacks.git branch laforge/stp instead of
    master, since it's only available in the former branch. Once it gets
    merged, we can modify Dockerfile as explained in it.
    
    OS#4220
    Change-Id: I635c891f5da7b8c59527abbad14d7377465d1045

---
+++ ttcn3-stp-test/jenkins.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+. ../jenkins-common.sh
+IMAGE_SUFFIX="${IMAGE_SUFFIX:-master}"
+docker_images_require \
+	"debian-stretch-build" \
+	"osmo-stp-$IMAGE_SUFFIX" \
+	"debian-stretch-titan" \
+	"ttcn3-stp-test"
+
+mkdir $VOL_BASE_DIR/stp-tester
+cp STP_Tests.cfg $VOL_BASE_DIR/stp-tester/
+
+mkdir $VOL_BASE_DIR/stp
+cp osmo-stp.cfg $VOL_BASE_DIR/stp/
+
+network_create 172.18.19.0/24
+
+echo Starting container with STP
+docker run	--rm \
+		--network $NET_NAME --ip 172.18.19.200 \
+		-v $VOL_BASE_DIR/stp:/data \
+		--name ${BUILD_TAG}-stp -d \
+		$REPO_USER/osmo-stp-$IMAGE_SUFFIX
+
+echo Starting container with STP testsuite
+docker run	--rm \
+		--network $NET_NAME --ip 172.18.19.203 \
+		-e "TTCN3_PCAP_PATH=/data" \
+		-v $VOL_BASE_DIR/stp-tester:/data \
+		--name ${BUILD_TAG}-ttcn3-stp-test \
+		$REPO_USER/ttcn3-stp-test
+
+docker container kill ${BUILD_TAG}-stp
+
+network_remove
+collect_logs
