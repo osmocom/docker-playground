0193073507459f51fcef1b66dc0a8cfb619fffe3
Pau Espin Pedrol <pespin@sysmocom.de>
2019-11-20 17:12:32 +0100

    respawn.sh: Add logging and limit loop iterations
    Since we'll now log the script and its children output to a file, let's
    make sure the loop doesn't run countless times upon failing child
    process, producing lots of potential undesired output. For instance, if
    osmo-bts fails to read vty config and exits immediately.
    
    Change-Id: Icc8cac889d94d4cce7d6365781d5b5364404b5b9

--- osmo-bts-master/respawn.sh
+++ osmo-bts-master/respawn.sh
@@ -2,8 +2,14 @@
 
 trap "kill 0" EXIT
 
-while [ -e /etc/passwd ]; do
+i=0
+max_i=500
+while [ $i -lt $max_i ]; do
+	echo "$i: starting: $*"
 	$* &
 	LAST_PID=$!
 	wait $LAST_PID
+	echo "$i: stopped pid $LAST_PID with status $?"
+	i=$(expr $i + 1)
 done
+echo "exiting after $max_i runs"
