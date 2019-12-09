osmocom/docker-playground

commit a66f6ebb23716a157ea68d78d6f7b827581dc5ed

ttcn3-bts: Mount trxcon dir so logs can be retrieved outside of docker
Change-Id: Icd243b62b8c80cdb2c2f09e1308f00565589c0e7

ttcn3-bts-test/jenkins.sh
@@ -56,6 +56,7 @@ start_trxcon() {
 	echo Starting container with trxcon
 	docker run	--rm \
 			--network $NET_NAME --ip 172.18.9.22 \
+			-v $VOL_BASE_DIR/trxcon:/data \
 			-v $VOL_BASE_DIR/unix:/data/unix \
 			--name ${BUILD_TAG}-trxcon -d \
 			$REPO_USER/osmocom-bb-host-master \

commit 1385d5e2be98d8c7a6f57d8d18df5198a6cbbae3

ttcn3-bts: Fix trxcon docker volume dir created as root
If directory is not created beforehand by the user, docker (root) will
create the directory before binding it to the container.

Related: OS#4313
Fixes: a66f6ebb23716a157ea68d78d6f7b827581dc5ed
Change-Id: I3f02faef1aa5846c48a3776191c3862bac913d10

ttcn3-bts-test/jenkins.sh
@@ -101,6 +101,7 @@ cp osmo-bts.cfg $VOL_BASE_DIR/bts/
 mkdir $VOL_BASE_DIR/unix
 
 mkdir $VOL_BASE_DIR/fake_trx
+mkdir $VOL_BASE_DIR/trxcon
 
 # 1) classic test suite with BSC for OML and trxcon+fake_trx
 start_bsc