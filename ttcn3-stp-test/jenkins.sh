8e13dc1d7f5beaaa5f6f87f2622323bd7e7f96b8
Harald Welte <laforge@osmocom.org>
2019-12-10 18:23:10 +0100

    ttcn3-stp-test: Support testing of 'latest'
    Change-Id: I1ea9d3312a2775aee8c298370fc5cd84a4b62477

--- ttcn3-stp-test/jenkins.sh
+++ ttcn3-stp-test/jenkins.sh
@@ -12,7 +12,11 @@ mkdir $VOL_BASE_DIR/stp-tester
 cp STP_Tests.cfg $VOL_BASE_DIR/stp-tester/
 
 mkdir $VOL_BASE_DIR/stp
-cp osmo-stp.cfg $VOL_BASE_DIR/stp/
+if [ "$IMAGE_SUFFIX" = "latest" ]; then
+	cp latest/osmo-stp.cfg $VOL_BASE_DIR/stp/
+else
+	cp osmo-stp.cfg $VOL_BASE_DIR/stp/
+fi
 
 network_create 172.18.19.0/24
 
