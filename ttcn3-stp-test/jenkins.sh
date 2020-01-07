osmocom/docker-playground

commit 0f34d3749192b12f8eb9ec678c5c834c6d599c4e

ttcn3-stp: Drop unneeded different osmo-stp.cfg for latest
Latest tagged release of libosmo-sccp, version 1.2.0, already supports
the regular osmo-stp.cfg file.

Change-Id: Ifb3eb642fa5547d1ce2de81f51f181e226dceed7

ttcn3-stp-test/jenkins.sh
@@ -12,11 +12,6 @@ mkdir $VOL_BASE_DIR/stp-tester
 cp STP_Tests.cfg $VOL_BASE_DIR/stp-tester/
 
 mkdir $VOL_BASE_DIR/stp
-if [ "$IMAGE_SUFFIX" = "latest" ]; then
-	cp latest/osmo-stp.cfg $VOL_BASE_DIR/stp/
-else
-	cp osmo-stp.cfg $VOL_BASE_DIR/stp/
-fi
 
 network_create 172.18.19.0/24
 

ttcn3-stp-test/latest/osmo-stp.cfg
@@ -1,112 +1,0 @@
-!
-! OsmoSTP (1.1.0.2-3884) configuration saved from vty
-!!
-!
-log gsmtap 172.18.19.203
- logging level set-all debug
- logging filter all 1
-log stderr
- logging filter all 1
- logging color 1
- logging print category 1
- logging timestamp 1
- logging print extended-timestamp 1
- logging level set-all debug
-!
-stats interval 5
-!
-line vty
- no login
- bind 0.0.0.0
-!
-cs7 instance 0
- point-code format 24
-!
-!M3UA AS/ASP:
-!
- asp asp-sender 9999 2905 m3ua
-  local-ip 172.18.19.200
-  remote-ip 172.18.19.203
- asp asp-receiver0 10000 2905 m3ua
-  local-ip 172.18.19.200
-  remote-ip 172.18.19.203
- asp asp-receiver1 10001 2905 m3ua
-  local-ip 172.18.19.200
-  remote-ip 172.18.19.203
- asp asp-client0 10002 2906 m3ua
-  local-ip 172.18.19.200
-  remote-ip 172.18.19.203
- as as-sender m3ua
-  asp asp-sender
-  routing-key 1023 23
- as as-receiver m3ua
-  asp asp-receiver0
-  asp asp-receiver1
-  routing-key 1042 42
- as as-client m3ua
-  routing-key 1055 55
-  asp asp-client0
-!
-! IPA AS/ASP:
-!
- asp ipa-asp-loadshare-sender 20000 5000 ipa
-  local-ip 172.18.19.200
-  remote-ip 172.18.19.203
-! LOADSHARE:
- as ipa-as-loadshare-sender ipa
-  routing-key 0 6
-  point-code override dpc 31
-  asp ipa-asp-loadshare-sender
- asp ipa-asp-loadshare-receiver0 20001 5000 ipa
-  local-ip 172.18.19.200
-  remote-ip 172.18.19.203
- asp ipa-asp-loadshare-receiver1 20002 5000 ipa
-  local-ip 172.18.19.200
-  remote-ip 172.18.19.203
- as ipa-as-loadshare-receiver ipa
-  traffic-mode loadshare
-  routing-key 0 7
-  point-code override dpc 32
-  asp ipa-asp-loadshare-receiver0
-  asp ipa-asp-loadshare-receiver1
-! OVERRIDE:
- asp ipa-asp-override-sender 20004 5000 ipa
-  local-ip 172.18.19.200
-  remote-ip 172.18.19.203
- as ipa-as-override-sender ipa
-  routing-key 0 6
-  point-code override dpc 33
-  asp ipa-asp-override-sender
- asp ipa-asp-override-receiver0 20005 5000 ipa
-  local-ip 172.18.19.200
-  remote-ip 172.18.19.203
- asp ipa-asp-override-receiver1 20006 5000 ipa
-  local-ip 172.18.19.200
-  remote-ip 172.18.19.203
- as ipa-as-override-receiver ipa
-  traffic-mode override
-  routing-key 0 8
-  point-code override dpc 34
-  asp ipa-asp-override-receiver0
-  asp ipa-asp-override-receiver1
-! DYNAMIC AS:
- as ipa-as-dynamic-asp ipa
-  routing-key 0 5
-  point-code override dpc 23
-!
-! ROUTING TABLE:
-!
- route-table system
-  update route 23 16777215 linkset as-sender
-  update route 42 16777215 linkset as-receiver
-  update route 55 16777215 linkset as-client
-  update route 31 16777215 linkset ipa-as-loadshare-receiver
-  update route 33 16777215 linkset ipa-as-override-receiver
-  update route 5  16777215 linkset ipa-as-dynamic-asp
-!
-!SERVERS:
-!
- listen m3ua 2905
-  accept-asp-connections dynamic-permitted
- listen ipa 5000
-  accept-asp-connections dynamic-permitted

commit 091f0a940a6b034f531fac22d955d5b23b44c9d8

ttcn3-stp: Fix recent commit breaking tests
Recent test aiming at dropping different cfg file for latest also
dropped the cp of the regular cfg file. Let's re-add it.

Fixes: 0f34d3749192b12f8eb9ec678c5c834c6d599c4e
Change-Id: Ia79e5b50978ed09ce4d0a5a286770e867657ad3e

ttcn3-stp-test/jenkins.sh
@@ -12,6 +12,7 @@ mkdir $VOL_BASE_DIR/stp-tester
 cp STP_Tests.cfg $VOL_BASE_DIR/stp-tester/
 
 mkdir $VOL_BASE_DIR/stp
+cp osmo-stp.cfg $VOL_BASE_DIR/stp/
 
 network_create 172.18.19.0/24
 