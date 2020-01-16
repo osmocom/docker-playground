b2f417d9a8da75b360ae3bf1966a3e10660e1606
Pau Espin Pedrol <pespin@sysmocom.de>
2020-01-16 16:24:05 +0100

    Introduce ttcn-sccp dockerized setup
    Since examples/sccp_demo_user (the binary used to test SCCP) is not
    installed by libosmo-sccp.git during make install, we need to install it
    manually after "make" step, and hence it's only available in
    osmo-stp-master, because in osmo-stp-latest we used packaged files.
    
    Related: OS#4343
    Depends: osmo-ttcn3-hacks.git Ied9fbfb9bd35a99205ca13db033dd9ea03d51238
    Change-Id: I5a7e6ff5f6e5f6e34d6a5ab29bd760a6f9d49c4b

---
+++ ttcn3-sccp-test/jenkins.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+. ../jenkins-common.sh
+IMAGE_SUFFIX="${IMAGE_SUFFIX:-master}"
+# Always require osmo-stp-master since is the only with sccp_demo_user installed
+docker_images_require \
+	"debian-stretch-build" \
+	"osmo-stp-master" \
+	"debian-stretch-titan" \
+	"ttcn3-sccp-test"
+
+mkdir $VOL_BASE_DIR/sccp-tester
+cp SCCP_Tests.cfg $VOL_BASE_DIR/sccp-tester/
+
+mkdir $VOL_BASE_DIR/sccp
+#cp osmo-sccp.cfg $VOL_BASE_DIR/sccp/
+
+network_create 172.18.22.0/24
+
+echo Starting container with sccp_demo_user
+docker run	--rm \
+		--network $NET_NAME --ip 172.18.22.200 \
+		-v $VOL_BASE_DIR/sccp:/data \
+		--name ${BUILD_TAG}-stp -d \
+		$REPO_USER/osmo-stp-master \
+		/bin/sh -c "sccp_demo_user -l 172.18.22.200 -r 172.18.22.203 >>/data/sccp_demo_user.log 2>&1"
+
+
+echo Starting container with SCCP testsuite
+docker run	--rm \
+		--network $NET_NAME --ip 172.18.22.203 \
+		-e "TTCN3_PCAP_PATH=/data" \
+		-v $VOL_BASE_DIR/sccp-tester:/data \
+		--name ${BUILD_TAG}-ttcn3-sccp-test \
+		$REPO_USER/ttcn3-sccp-test
+
+docker container kill ${BUILD_TAG}-stp
+
+network_remove
+collect_logs
