9145a91594e4dd5f987a9d94b36a087af41fb25c
Pau Espin Pedrol <pespin@sysmocom.de>
2020-03-11 16:33:59 +0100

    osmo-gsm-tester: Allow passing osmo-gsm-tester params from jenkins.sh caller
    After this patch, one can run osmo-gsm-tester with specific parameters
    (debug level, suite and tests to run, etc.) by using
    OSMO_GSM_TESTER_OPTS variable:
    OSMO_GSM_TESTER_OPTS="-s 4g -t ping -l error" ./jenkins.sh
    
    Change-Id: I2e8ff51a3152125148c6a117a987e8c11a576e1e

--- osmo-gsm-tester/osmo-gsm-tester-master.sh
+++ osmo-gsm-tester/osmo-gsm-tester-master.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+set -x -e
 # Add local IP addresses required by osmo-gsm-tester resources:
-ip addr add 172.18.50.2/24 dev eth0
+ip addr add 172.18.50.2/24 dev eth0 || true #already set by docker run --ip cmd
 ip addr add 172.18.50.3/24 dev eth0
 ip addr add 172.18.50.4/24 dev eth0
 ip addr add 172.18.50.5/24 dev eth0
@@ -11,4 +11,4 @@ ip addr add 172.18.50.8/24 dev eth0
 ip addr add 172.18.50.9/24 dev eth0
 ip addr add 172.18.50.10/24 dev eth0
 
-su -c "python3 -u /tmp/osmo-gsm-tester/src/osmo-gsm-tester.py /tmp/trial -T -l dbg -s 4g -t iperf3" -m jenkins
+su -c "python3 -u /tmp/osmo-gsm-tester/src/osmo-gsm-tester.py /tmp/trial $OSMO_GSM_TESTER_OPTS" -m jenkins
