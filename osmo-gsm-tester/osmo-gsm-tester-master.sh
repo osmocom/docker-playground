41230705dcccd2010dc7a748dfaa8cc6fb2c3f91
Pau Espin Pedrol <pespin@sysmocom.de>
2020-03-19 20:11:56 +0100

    osmo-gsm-tester: Build srsLTE inside the docker instance if inst not provided by host
    osmo-gsm-tester.git/contrib/jenkins-build-srslte.sh cannot be used
    because there's a need to split the fetch step from the build step.
    That's because we want to fetch in the host (in order to use host's user
    ssh key for potential private repos) while still building inside the
    docker container, where we have all the dependencies (and where the
    binaries are going to be run afterwards by osmo-gsm-tester).
    
    During the changes above it was also noticed that /tmp/trial on the host
    got its UID+GID changed (because we switch user inside the container to
    downscale privileges). Fix it by chown'ing the directory back to the
    host user's UID+GID.
    
    Change-Id: Ia0db8fba840392a0b7883cfc15e45c2e80407a16

--- osmo-gsm-tester/osmo-gsm-tester-master.sh
+++ osmo-gsm-tester/osmo-gsm-tester-master.sh
@@ -11,4 +11,39 @@ ip addr add 172.18.50.8/24 dev eth0
 ip addr add 172.18.50.9/24 dev eth0
 ip addr add 172.18.50.10/24 dev eth0
 
-su -c "python3 -u /tmp/osmo-gsm-tester/src/osmo-gsm-tester.py /tmp/trial $OSMO_GSM_TESTER_OPTS" -m jenkins
+build_srslte() {
+        git_repo_dir="/tmp/trial/${SRS_LTE_REPO_NAME}"
+        if [ ! -d "$git_repo_dir" ]; then
+                echo "No external trial nor git repo provided for srsLTE!"
+                exit 1
+        fi
+        pushd "/tmp/trial"
+        rm -rf inst && mkdir inst
+        rm -rf build && mkdir build && cd build || exit 1
+        cmake -DCMAKE_INSTALL_PREFIX="../inst/" "${git_repo_dir}"
+        set +x; echo; echo; set -x
+        make "-j$(nproc)"
+        set +x; echo; echo; set -x
+        make install
+        this="srslte.build-${BUILD_NUMBER-$(date +%Y-%m-%d_%H_%M_%S)}"
+        tar="${this}.tgz"
+        tar czf "/tmp/trial/$tar" -C "/tmp/trial/inst" .
+        cd "/tmp/trial/" && md5sum "$tar" >>checksums.md5
+        popd
+}
+
+# Build srsLTE.git if not provided by host system:
+if [ "x$(ls /tmp/trial/srslte.*.tgz 2>/dev/null | wc -l)" = "x0" ]; then
+        build_srslte
+fi
+
+# Make trial dir avaialable to jenkins user inside container:
+chown -R jenkins /tmp/trial/
+
+rc=0
+su -c "python3 -u /tmp/osmo-gsm-tester/src/osmo-gsm-tester.py /tmp/trial $OSMO_GSM_TESTER_OPTS" -m jenkins || rc=$?
+
+# Make trial dir again owned by user running the container:
+chown -R "${HOST_USER_ID}:${HOST_GROUP_ID}" /tmp/trial/
+
+exit $rc
