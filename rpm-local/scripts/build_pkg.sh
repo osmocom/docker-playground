0cf5948f4db7cfecc991c490e0435cc35bcaf570
Oliver Smith <osmith@sysmocom.de>
2020-05-07 15:04:10 +0200

    rpm-local: resolve depends of previously built pkgs
    Change-Id: Ic402ae3a3b82086c090e638c56daa5fb9d39888f

--- rpm-local/scripts/build_pkg.sh
+++ rpm-local/scripts/build_pkg.sh
@@ -1,9 +1,24 @@
 #!/bin/sh -ex
 # $1: pkgname
+ARCH="x86_64"
 
-cd /home/user/rpmbuild/SPECS
+cd /home/user/rpmbuild/RPMS
+
+# if already built packages exist, run createrepo and add it as local repo
+if [ -d "$ARCH" ]; then
+	cd "$ARCH"
+	createrepo .
+
+	cat <<- EOF > /etc/yum.repos.d/rpmbuild-local.repo
+	[myrepo]
+	name=Local Osmocom packages
+	baseurl=file:///home/user/rpmbuild/RPMS/$ARCH
+	enabled=1
+	EOF
+fi
 
 # keepcache: /var/cache/dnf is mounted from outside docker dir, so downloaded rpm depends are cached
+cd /home/user/rpmbuild/SPECS
 dnf \
 	--setopt=keepcache=1 \
 	-y \
