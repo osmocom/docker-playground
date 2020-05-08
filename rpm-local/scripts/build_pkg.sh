f6ccb836279b46983b8eaed8504f1530c925dd60
Oliver Smith <osmith@sysmocom.de>
2020-05-08 10:42:22 +0200

    build_pkg.sh: install systemd-rpm-macros
    systemd-rpm-macros is a noarch package, so make sure to also index
    those.
    
    Change-Id: I3705ebfbabc8b05ea7e95426501564e1e6776663

--- rpm-local/scripts/build_pkg.sh
+++ rpm-local/scripts/build_pkg.sh
@@ -1,21 +1,30 @@
 #!/bin/sh -ex
 # $1: pkgname
-ARCH="x86_64"
+ARCHES="x86_64 noarch"
 
 cd /home/user/rpmbuild/RPMS
 
 # if already built packages exist, run createrepo and add it as local repo
-if [ -d "$ARCH" ]; then
-	cd "$ARCH"
-	createrepo .
+for arch in $ARCHES; do
+	if [ -d "$arch" ]; then
+		cd "$arch"
+		createrepo .
+	
+		cat <<- EOF > /etc/yum.repos.d/rpmbuild-local-$arch.repo
+		[myrepo-$arch]
+		name=Local Osmocom packages ($arch)
+		baseurl=file:///home/user/rpmbuild/RPMS/$arch
+		enabled=1
+		gpgcheck=0
+		EOF
 
-	cat <<- EOF > /etc/yum.repos.d/rpmbuild-local.repo
-	[myrepo]
-	name=Local Osmocom packages
-	baseurl=file:///home/user/rpmbuild/RPMS/$ARCH
-	enabled=1
-	gpgcheck=0
-	EOF
+		cd ..
+	fi
+done
+
+# HACK: install systemd-rpm-macros unless we are building it now, so the spec files can be parsed
+if [ "$1" != "systemd-rpm-macros" ]; then
+	dnf -y install systemd-rpm-macros
 fi
 
 # keepcache: /var/cache/dnf is mounted from outside docker dir, so downloaded rpm depends are cached
