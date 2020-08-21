8d4a97a3fc9fb75fd2edbdc5c5d50df93a761b1f
Pau Espin Pedrol <pespin@sysmocom.de>
2020-08-21 17:57:02 +0200

    ttcn3-stp: Configure M3UA setup to use IPv6 together with IPv4
    Depends: libosmo-sccp.git Change-Id I09226a5cecc37dd4676acd61c2051befe5234cb3
    Depends: osmo-ttcn3-hacks.git Change-Id Ib1925ed1df5cea3fa66f28b5625532d454a2c338
    Change-Id: Id6e197cc3ba71e36f29ceec44742b7452a47b36b

--- ttcn3-stp-test/jenkins.sh
+++ ttcn3-stp-test/jenkins.sh
@@ -12,11 +12,18 @@ cp STP_Tests.cfg $VOL_BASE_DIR/stp-tester/
 mkdir $VOL_BASE_DIR/stp
 cp osmo-stp.cfg $VOL_BASE_DIR/stp/
 
+# Disable IPv6 until libosmo-sccp.git release > 1.3.0 is available
+if [ "$IMAGE_SUFFIX" = "latest" ]; then
+	sed "/fd02:db8/d" -i "$VOL_BASE_DIR/stp/osmo-stp.cfg"
+	sed 's/, "fd02:db8:19::203"//g' -i "$VOL_BASE_DIR/stp-tester/STP_Tests.cfg"
+	sed 's/, "fd02:db8:19::200"//g' -i "$VOL_BASE_DIR/stp-tester/STP_Tests.cfg"
+fi
+
 network_create 19
 
 echo Starting container with STP
 docker run	--rm \
-		--network $NET_NAME --ip 172.18.19.200 \
+		--network $NET_NAME --ip 172.18.19.200 --ip6 fd02:db8:19::200 \
 		--ulimit core=-1 \
 		-v $VOL_BASE_DIR/stp:/data \
 		--name ${BUILD_TAG}-stp -d \
@@ -25,7 +32,7 @@ docker run	--rm \
 
 echo Starting container with STP testsuite
 docker run	--rm \
-		--network $NET_NAME --ip 172.18.19.203 \
+		--network $NET_NAME --ip 172.18.19.203 --ip6 fd02:db8:19::203 \
 		--ulimit core=-1 \
 		-e "TTCN3_PCAP_PATH=/data" \
 		-v $VOL_BASE_DIR/stp-tester:/data \
