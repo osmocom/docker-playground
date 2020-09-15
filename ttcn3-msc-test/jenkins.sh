be98438d941622ac1250e582f3405bde1fe2612a
Pau Espin Pedrol <pespin@sysmocom.de>
2020-09-15 16:04:55 +0200

    ttcn3-msc: Update module params against new osmo-ttcn3-hacks.git
    module param mp_enable_osmux_test is dropped in osmo-ttcn3-hacks.git
    I8bc0551ec91a5fd8ea2f291a1e16a06a739c7a75.
    module param mp_enable_crashing_tests is introduced in
    osmo-ttcn3-hacks.git I46f1066323e19dfe708402a8c9c68e257f62751c.
    
    Change-Id: I22bb43ba6d15eabcc731a2c3ef7aabc4f250d508

--- ttcn3-msc-test/jenkins.sh
+++ ttcn3-msc-test/jenkins.sh
@@ -20,6 +20,8 @@ if [ "$IMAGE_SUFFIX" = "latest" ]; then
 		"$VOL_BASE_DIR/msc-tester/MSC_Tests.cfg"
 	sed "s/BSC_ConnectionHandler.mp_expect_common_id := true/BSC_ConnectionHandler.mp_expect_common_id := false/" -i \
 		"$VOL_BASE_DIR/msc-tester/MSC_Tests.cfg"
+	sed "s/MSC_Tests.mp_enable_crashing_tests := true/MSC_Tests.mp_enable_crashing_tests := false/" -i \
+		"$VOL_BASE_DIR/msc-tester/MSC_Tests.cfg"
 fi
 
 mkdir $VOL_BASE_DIR/stp
