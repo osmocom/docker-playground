2d831f1b50ef82c068098393f748ff44715a965c
Harald Welte <laforge@osmocom.org>
2021-03-02 12:30:24 +0100

    Add osmo-gbproxy-latest container
    Change-Id: I285cb51da03b793eab994ff3c00de16097e3f992

---
+++ osmo-gbproxy-latest/docker-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+set -x
+
+if [[ -n ${WAIT_FOR_NETDEV:-} ]]; then
+	/usr/bin/pipework --wait -i ${WAIT_FOR_NETDEV}
+fi
+
+/usr/bin/osmo-gbproxy -c /data/osmo-gbproxy.cfg >/data/osmo-gbproxy.log 2>&1
