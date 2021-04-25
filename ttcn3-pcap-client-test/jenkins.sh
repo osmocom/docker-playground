11591e496e96006d222a2de8dd4907182a4e04fd
Harald Welte <laforge@osmocom.org>
2021-04-25 11:10:01 +0000

    Add ttcn3-pcap-client-test
    This runs pcap-client IUT against the TTCN3 ATS for pcap-client
    
    Change-Id: Ie94106b1d1f348cd66f4b5304be6021dbaf732e1

---
+++ ttcn3-pcap-client-test/jenkins.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+. ../jenkins-common.sh
+IMAGE_SUFFIX="${IMAGE_SUFFIX:-master}"
+docker_images_require \
+	"osmo-pcap-$IMAGE_SUFFIX" \
+	"ttcn3-pcap-client-test"
+
+set_clean_up_trap
+set -e
+
+mkdir $VOL_BASE_DIR/pcap-client-tester
+cp OPCAP_CLIENT_Tests.cfg $VOL_BASE_DIR/pcap-client-tester/
+
+mkdir $VOL_BASE_DIR/pcap-client
+cp osmo-pcap-client.cfg $VOL_BASE_DIR/pcap-client/
+
+SUBNET=31
+network_create $SUBNET
+
+echo Starting container with pcap-client
+docker run	--rm \
+		$(docker_network_params $SUBNET 20) \
+		--ulimit core=-1 \
+		-v $VOL_BASE_DIR/pcap-client:/data \
+		--name ${BUILD_TAG}-pcap-client -d \
+		$DOCKER_ARGS \
+		$REPO_USER/osmo-pcap-$IMAGE_SUFFIX
+
+echo Starting container with pcap-client testsuite
+docker run	--rm \
+		$(docker_network_params $SUBNET 10) \
+		--ulimit core=-1 \
+		-e "TTCN3_PCAP_PATH=/data" \
+		-v $VOL_BASE_DIR/pcap-client-tester:/data \
+		--name ${BUILD_TAG}-ttcn3-pcap-client-test \
+		$DOCKER_ARGS \
+		$REPO_USER/ttcn3-pcap-client-test
