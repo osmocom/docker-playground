9f9782a702f3f275f64df614de0813e93d0e0ecd
Oliver Smith <osmith@sysmocom.de>
2021-05-20 14:21:24 +0200

    ttcn3-sgsn-test: fix config sed for latest
    Change-Id: I17d0554188fad6f890f844bf978d622661290fce

--- ttcn3-sgsn-test/jenkins.sh
+++ ttcn3-sgsn-test/jenkins.sh
@@ -26,7 +26,7 @@ mkdir $VOL_BASE_DIR/unix
 
 # Disable until osmo-sgsn.git release > 1.7.0 is available
 if [ "$IMAGE_SUFFIX" = "latest" ]; then
-	sed "/mme test-mme0/d" -i $VOL_BASE_DIR/stp/osmo-sgsn.cfg
+	sed "/mme test-mme0/d" -i $VOL_BASE_DIR/sgsn/osmo-sgsn.cfg
 	sed "/gtp remote-ip/d" -i $VOL_BASE_DIR/sgsn/osmo-sgsn.cfg
 	sed "/gtp ran-info-relay/d" -i $VOL_BASE_DIR/sgsn/osmo-sgsn.cfg
 fi
