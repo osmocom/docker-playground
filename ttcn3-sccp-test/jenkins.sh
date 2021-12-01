osmocom/docker-playground

commit 305ce55b749cdf3f202639ee728ad20d9f18a89b

ttcn3: write Osmocom repository to configs
Write a line like 'Misc_Helpers.mp_osmo_repo := "nightly"' into the
TTCN-3 config file (e.g. BSC_Tests.cfg), before starting the testsuite.
This allows executing different code paths in the tests based on the
repository.

Related: OS#5327
Change-Id: Ic06532f7a67e59458652c5cf4c8f6fee8113e703

ttcn3-sccp-test/jenkins.sh
@@ -12,6 +12,7 @@ set -e
 
 mkdir $VOL_BASE_DIR/sccp-tester
 cp SCCP_Tests.cfg $VOL_BASE_DIR/sccp-tester/
+write_mp_osmo_repo "$VOL_BASE_DIR/scpp-tester/SCCP_Tests.cfg"
 
 mkdir $VOL_BASE_DIR/sccp
 cp sccp_demo_user.cfg $VOL_BASE_DIR/sccp/

ttcn3-bsc-test-vamos/jenkins.sh
@@ -13,6 +13,7 @@ set -e
 
 mkdir $VOL_BASE_DIR/bsc-tester
 cp BSC_Tests.cfg $VOL_BASE_DIR/bsc-tester/
+write_mp_osmo_repo "$VOL_BASE_DIR/bsc-tester/BSC_Tests.cfg"
 
 mkdir $VOL_BASE_DIR/stp
 cp osmo-stp.cfg $VOL_BASE_DIR/stp/

ttcn3-bsc-test/jenkins.sh
@@ -13,6 +13,7 @@ set -e
 
 mkdir $VOL_BASE_DIR/bsc-tester
 cp BSC_Tests.cfg $VOL_BASE_DIR/bsc-tester/
+write_mp_osmo_repo "$VOL_BASE_DIR/bsc-tester/BSC_Tests.cfg"
 
 mkdir $VOL_BASE_DIR/stp
 cp osmo-stp.cfg $VOL_BASE_DIR/stp/

ttcn3-bscnat-test/jenkins.sh
@@ -11,6 +11,7 @@ set -e
 
 mkdir $VOL_BASE_DIR/bscnat-tester
 cp BSCNAT_Tests.cfg $VOL_BASE_DIR/bscnat-tester/
+write_mp_osmo_repo "$VOL_BASE_DIR/bscnat-tester/BSCNAT_Tests.cfg"
 
 mkdir $VOL_BASE_DIR/bscnat
 cp osmo-bsc-nat.cfg $VOL_BASE_DIR/bscnat/

ttcn3-bts-test/jenkins.sh
@@ -119,12 +119,16 @@ network_create $SUBNET
 
 mkdir $VOL_BASE_DIR/bts-tester-generic
 cp BTS_Tests.cfg $VOL_BASE_DIR/bts-tester-generic/
+write_mp_osmo_repo "$VOL_BASE_DIR/bts-tester-generic/BTS_Tests.cfg"
 mkdir $VOL_BASE_DIR/bts-tester-virtphy
 cp virtphy/BTS_Tests.cfg $VOL_BASE_DIR/bts-tester-virtphy/
+write_mp_osmo_repo "$VOL_BASE_DIR/bts-tester-virtphy/BTS_Tests.cfg"
 mkdir $VOL_BASE_DIR/bts-tester-oml
 cp oml/BTS_Tests.cfg $VOL_BASE_DIR/bts-tester-oml/
+write_mp_osmo_repo "$VOL_BASE_DIR/bts-tester-oml/BTS_Tests.cfg"
 mkdir $VOL_BASE_DIR/bts-tester-hopping
 cp fh/BTS_Tests.cfg $VOL_BASE_DIR/bts-tester-hopping/
+write_mp_osmo_repo "$VOL_BASE_DIR/bts-tester-hopping/BTS_Tests.cfg"
 
 cp $VOL_BASE_DIR/bts-tester-generic/BTS_Tests.cfg \
    $VOL_BASE_DIR/bts-tester-hopping/BTS_Tests.cfg.inc

ttcn3-cbc-test/jenkins.sh
@@ -11,6 +11,7 @@ set -e
 
 mkdir $VOL_BASE_DIR/cbc-tester
 cp CBC_Tests.cfg $VOL_BASE_DIR/cbc-tester/
+write_mp_osmo_repo "$VOL_BASE_DIR/cbc-tester/CBC_Tests.cfg"
 
 mkdir $VOL_BASE_DIR/cbc
 cp osmo-cbc.cfg $VOL_BASE_DIR/cbc/

ttcn3-fr-test/jenkins.sh
@@ -16,6 +16,7 @@ mkdir $VOL_BASE_DIR/fr-tester
 chmod a+w $VOL_BASE_DIR/fr-tester
 
 cp FR_Tests.cfg $VOL_BASE_DIR/fr-tester/
+write_mp_osmo_repo "$VOL_BASE_DIR/fr-tester/FR_Tests.cfg"
 
 mkdir $VOL_BASE_DIR/frnet
 cp FRNET_Tests.cfg $VOL_BASE_DIR/frnet/

ttcn3-gbproxy-test-fr/jenkins.sh
@@ -29,6 +29,7 @@ mkdir $VOL_BASE_DIR/gbproxy-tester
 # if we don't change permissions, dumpcap fails to write (despite starting it as root!)
 chmod a+w $VOL_BASE_DIR/gbproxy-tester
 cp GBProxy_Tests.cfg $VOL_BASE_DIR/gbproxy-tester/
+write_mp_osmo_repo "$VOL_BASE_DIR/gbproxy-tester/GBProxy_Tests.cfg"
 
 mkdir $VOL_BASE_DIR/gbproxy
 cp osmo-gbproxy.cfg $VOL_BASE_DIR/gbproxy/

commit a7f0d71a764094c09d2a583d4e7a97b914c7b69c

ttcn3-sccp-test/jenkins.sh: fix typo
Fixes: 305ce55 ("ttcn3: write Osmocom repository to configs")
Change-Id: I79734da2744502a2b8c4db9d38f992b77fe6a42a

ttcn3-sccp-test/jenkins.sh
@@ -12,7 +12,7 @@ set -e
 
 mkdir $VOL_BASE_DIR/sccp-tester
 cp SCCP_Tests.cfg $VOL_BASE_DIR/sccp-tester/
-write_mp_osmo_repo "$VOL_BASE_DIR/scpp-tester/SCCP_Tests.cfg"
+write_mp_osmo_repo "$VOL_BASE_DIR/sccp-tester/SCCP_Tests.cfg"
 
 mkdir $VOL_BASE_DIR/sccp
 cp sccp_demo_user.cfg $VOL_BASE_DIR/sccp/