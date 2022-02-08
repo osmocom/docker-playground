45a2b5d817605fe0a7d5ce5207d2cad0d63dde69
Pau Espin Pedrol <pespin@sysmocom.de>
2022-02-08 18:06:37 +0100

    ttcn3-ggsn-tests-ogs: Configuration fixes for IPv6 tests
    Change-Id: I6fe98f6642bbd42376c9ed7353b58451c104eaca

--- ttcn3-ggsn-test/ogs/upfd-setup.sh
+++ ttcn3-ggsn-test/ogs/upfd-setup.sh
@@ -19,9 +19,9 @@ create_tun "ogstun6"
 create_tun "ogstun46"
 
 add_addr "ogstun4" "176.16.222.1/16"
-add_addr "ogstun6" "2001:780:44:2000:0:0:0:1/64"
+add_addr "ogstun6" "2001:780:44:2000:0:0:0:1/56"
 add_addr "ogstun46" "176.16.46.1/24"
-add_addr "ogstun46" "2001:780:44:2100:0:0:0:1/64"
+add_addr "ogstun46" "2001:780:44:2100:0:0:0:1/56"
 
 ip link set ogstun4 up
 ip link set ogstun6 up
