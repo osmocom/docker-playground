cc7f9b48969d139a4524ed46a8099d9386b3248f
Pau Espin Pedrol <pespin@sysmocom.de>
2022-04-07 19:34:50 +0200

    ttcn3-ggsn-ogs: Rename config files
    Let's make names similar to what's used in PGW_Tests. Also, in
    open5gs.git, config files don't contain the "d" at the end.
    
    Change-Id: If56d1a890a4f1b80b4f04ae22644f9cb9cdd35f4

--- ttcn3-ggsn-test/jenkins-ogs.sh
+++ ttcn3-ggsn-test/jenkins-ogs.sh
@@ -19,8 +19,7 @@ cp ogs/GGSN_Tests.cfg $VOL_BASE_DIR/ggsn-tester/
 write_mp_osmo_repo "$VOL_BASE_DIR/ggsn-tester/GGSN_Tests.cfg"
 
 mkdir $VOL_BASE_DIR/ggsn
-cp ogs/smfd.yaml $VOL_BASE_DIR/ggsn/
-cp ogs/upfd.yaml $VOL_BASE_DIR/ggsn/
+cp ogs/open5gs-*.yaml $VOL_BASE_DIR/ggsn/
 cp ogs/freediameter.conf $VOL_BASE_DIR/ggsn/
 cp ogs/upfd.sh $VOL_BASE_DIR/ggsn/
 cp ogs/upfd-setup.sh $VOL_BASE_DIR/ggsn/
@@ -39,7 +38,7 @@ docker run	--cap-add=NET_ADMIN \
 		$DOCKER_ARGS \
 		$(docker_network_params $SUBNET 201) \
 		$REPO_USER/open5gs-$IMAGE_SUFFIX \
-		/bin/sh -c "open5gs-smfd -c /data/smfd.yaml >/data/open5gs-smfd.out 2>&1"
+		/bin/sh -c "open5gs-smfd -c /data/open5gs-smf.yaml >/data/open5gs-smfd.out 2>&1"
 
 docker run	--cap-add=NET_ADMIN \
 		--device /dev/net/tun:/dev/net/tun \
@@ -51,7 +50,7 @@ docker run	--cap-add=NET_ADMIN \
 		$DOCKER_ARGS \
 		$(docker_network_params $SUBNET 222) \
 		$REPO_USER/open5gs-$IMAGE_SUFFIX \
-		/bin/sh -c "/data/upfd.sh -c /data/upfd.yaml >/data/open5gs-upfd.out 2>&1"
+		/bin/sh -c "/data/upfd.sh -c /data/open5gs-upf.yaml >/data/open5gs-upfd.out 2>&1"
 
 # start docker container with testsuite in foreground
 docker run	--rm \
@@ -63,4 +62,3 @@ docker run	--rm \
 		--name ${BUILD_TAG}-ggsn-test \
 		$DOCKER_ARGS \
 		$REPO_USER/ttcn3-ggsn-test
-
