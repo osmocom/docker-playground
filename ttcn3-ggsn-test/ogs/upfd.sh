6e1a4e7df04f1f9086902bb68891168cd7673025
Pau Espin Pedrol <pespin@sysmocom.de>
2022-05-02 11:33:53 +0200

    ttcn3-ggsn-test-ogs: set CAP_NET_RAW for open5gs-upfd
    The SO_BINDTODEVICE feature (used for VRF) requires CAP_NET_RAW. Since
    we run open5gs-upfd as user "osmocom", that seems to be causing some
    permission problems under some systems (like jenkins). Let's make sure
    we add the capabilitites to the binary before launching it as user
    "osmocom".
    
    Change-Id: I51ee6954a6c019a41cfcd50b2d99166316989d9b

--- ttcn3-ggsn-test/ogs/upfd.sh
+++ ttcn3-ggsn-test/ogs/upfd.sh
@@ -2,5 +2,7 @@
 set -e
 set -x
 /data/upfd-setup.sh
-#du -lha / | grep freeDiameter
-su - osmocom -c "open5gs-upfd $*"
+upfd_bin="$(command -v open5gs-upfd)"
+# so_bindtodevice cfg requires CAP_NET_RAW:
+setcap cap_net_raw+ep "$upfd_bin"
+su - osmocom -c "$upfd_bin $*"
