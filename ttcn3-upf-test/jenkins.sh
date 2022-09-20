6f366b30a970fb16111ca52af336780c27675156
Neels Hofmeyr <nhofmeyr@sysmocom.de>
2022-09-20 12:56:05 +0200

    add ttcn3-upf-test
    Related: I43cd55c9395f2c706369afe1a9a7f475be47339f (osmo-ci)
    Related: I5964979b3db86268f6011df0afbee7131557f405 (osmo-ci)
    Change-Id: I7a651bb85861de4b2677a731b9f17fa0d790c621

---
+++ ttcn3-upf-test/jenkins.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+. ../jenkins-common.sh
+IMAGE_SUFFIX="${IMAGE_SUFFIX:-master}"
+docker_images_require \
+	"osmo-upf-$IMAGE_SUFFIX" \
+	"ttcn3-upf-test"
+
+set_clean_up_trap
+set -e
+
+mkdir $VOL_BASE_DIR/upf-tester
+cp UPF_Tests.cfg $VOL_BASE_DIR/upf-tester/
+write_mp_osmo_repo "$VOL_BASE_DIR/upf-tester/UPF_Tests.cfg"
+
+mkdir $VOL_BASE_DIR/upf
+cp osmo-upf.cfg $VOL_BASE_DIR/upf/
+
+SUBNET=34
+network_create $SUBNET
+
+echo Starting container with UPF
+docker run	--rm \
+		$(docker_network_params $SUBNET 20) \
+		--ulimit core=-1 \
+		-v $VOL_BASE_DIR/upf:/data \
+		--name ${BUILD_TAG}-upf -d \
+		$DOCKER_ARGS \
+		$REPO_USER/osmo-upf-$IMAGE_SUFFIX
+
+echo Starting container with UPF testsuite
+docker run	--rm \
+		$(docker_network_params $SUBNET 203) \
+		--ulimit core=-1 \
+		-e "TTCN3_PCAP_PATH=/data" \
+		-v $VOL_BASE_DIR/upf-tester:/data \
+		--name ${BUILD_TAG}-ttcn3-upf-test \
+		$DOCKER_ARGS \
+		$REPO_USER/ttcn3-upf-test
