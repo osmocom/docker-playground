osmocom/docker-playground

commit e077fec68419d13c2dbe347b6799f407dd927ffd

ttcn3-mgw: Set osmux bind-ip(-v6) VTY configs
Until now we forgot to properly configure the osmux socket local ip, but
that was fine because 0.0.0.0 was taken as a default.
With addition of IPv6, this changed, and the socket is not bound unless
an IP address is set (to allow conditional use of IPv4, IPv6 or both).

Depends: osmo-mgw.git Change-Id I446cd7da217e9f4a74995d7784ae55dcc60a29b7
Change-Id: Ib7e9428a10238fae395a1707d6034dc5c97b9024

ttcn3-mgw-test/jenkins.sh
@@ -15,6 +15,10 @@ write_mp_osmo_repo "$VOL_BASE_DIR/mgw-tester/MGCP_Test.cfg"
 
 mkdir $VOL_BASE_DIR/mgw
 cp osmo-mgw.cfg $VOL_BASE_DIR/mgw/
+# Can be dropped once released osmo-mgw is >1.10.0:
+if image_suffix_is_latest; then
+	sed -i "/^ osmux bind-ip-v6 fd02:db8:4::180/d" $VOL_BASE_DIR/mgw/osmo-mgw.cfg
+fi
 
 SUBNET=4
 network_create $SUBNET

ttcn3-mgw-test/osmo-mgw.cfg
@@ -58,6 +58,8 @@ mgcp
  number endpoints 31
  allow-transcoding
  osmux off
+ osmux bind-ip 172.18.4.180
+ osmux bind-ip-v6 fd02:db8:4::180
  trunk 1
   sdp audio-payload send-ptime
   sdp audio-payload send-name

commit 8bf80576689d26de00b6c74d0372dc6e620123d0

ttcn3-mgw: jenkins.sh Fix centos tests
The sed lines dropping the new configs must be applied to any non-master
run.

Fixes: e077fec68419d13c2dbe347b6799f407dd927ffd
Change-Id: I369bb61f3979bd9118fad17c3d202253411ba14b

ttcn3-mgw-test/jenkins.sh
@@ -16,7 +16,7 @@ write_mp_osmo_repo "$VOL_BASE_DIR/mgw-tester/MGCP_Test.cfg"
 mkdir $VOL_BASE_DIR/mgw
 cp osmo-mgw.cfg $VOL_BASE_DIR/mgw/
 # Can be dropped once released osmo-mgw is >1.10.0:
-if image_suffix_is_latest; then
+if ! image_suffix_is_master; then
 	sed -i "/^ osmux bind-ip-v6 fd02:db8:4::180/d" $VOL_BASE_DIR/mgw/osmo-mgw.cfg
 fi
 