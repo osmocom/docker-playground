osmocom/docker-playground

commit 62ac21133a828bab849c072f27664102790fcb91

jenkins-common: docker_depends: add open5gs
Change-Id: Ibcca8cf399abb4a4778318d1743985da8afde185

jenkins-common.sh
@@ -21,6 +21,7 @@ docker_depends() {
 	osmo-*-centos8) echo "centos8-build" ;;
 	osmo-*-latest) echo "debian-bullseye-obs-latest" ;;
 	osmo-*) echo "debian-bullseye-build" ;;
+	open5gs-*) echo "debian-bullseye-build" ;;
 	ttcn3-*) echo "debian-bullseye-titan" ;;
 	esac
 }

commit 4a2d9d1c0c1eb8c3e957d78399db54004b69115b

jenkins-common.sh: open5gs-latest does not require debian-bullseye-build
We're not *building* anything for the open5gs-latest, but simply
downloading and installing DEBs from Sukchan's opensuse.org repo.

Change-Id: Iec7ccab28c37d274a731e08a2f893ec0514c1068
Fixes: 62ac21133a828bab849c072f27664102790fcb91

jenkins-common.sh
@@ -21,7 +21,7 @@ docker_depends() {
 	osmo-*-centos8) echo "centos8-build" ;;
 	osmo-*-latest) echo "debian-bullseye-obs-latest" ;;
 	osmo-*) echo "debian-bullseye-build" ;;
-	open5gs-*) echo "debian-bullseye-build" ;;
+	open5gs-master) echo "debian-bullseye-build" ;;
 	ttcn3-*) echo "debian-bullseye-titan" ;;
 	esac
 }