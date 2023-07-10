c2b05caf6e13624d13e21dec6698f87ea5dbff34
Neels Hofmeyr <nhofmeyr@sysmocom.de>
2023-07-10 23:43:33 +0200

    hnbgw: adjust cfg for cnpool tests
    Config changes matching cnpool tests added in osmo-ttcn3-hacks, see
    'Related'.
    
    Keep a copy of the old config files named "-legacy", to not break the
    'latest' tests, because osmo-hnbgw 'latest' does not yet support the new
    config options.
    
    Depends: osmo-ttcn3-hacks I027a059faed3f140f8801f84338956cd004043b5
    Change-Id: I94aa0b2adfc48b98cb4b1efe595c2432fc603d6c

--- ttcn3-hnbgw-test/jenkins.sh
+++ ttcn3-hnbgw-test/jenkins.sh
@@ -82,9 +82,19 @@ run_tests() {
 	docker_kill_wait ${BUILD_TAG}-stp
 }
 
+osmo_stp_cfg="osmo-stp.cfg"
+osmo_hnbgw_cfg="osmo-hnbgw.cfg"
+osmo_hnbgw_with_pfcp_cfg="with-pfcp/osmo-hnbgw.cfg"
+
+if image_suffix_is_latest; then
+	osmo_stp_cfg="osmo-stp-legacy.cfg"
+	osmo_hnbgw_cfg="osmo-hnbgw-legacy.cfg"
+	osmo_hnbgw_with_pfcp_cfg="with-pfcp/osmo-hnbgw-legacy.cfg"
+fi
+
 echo Testing without PFCP
-run_tests "$VOL_BASE_DIR" "HNBGW_Tests.cfg" "osmo-stp.cfg" "osmo-hnbgw.cfg"
+run_tests "$VOL_BASE_DIR" "HNBGW_Tests.cfg" $osmo_stp_cfg $osmo_hnbgw_cfg
 
 echo Testing with PFCP
 mkdir "$VOL_BASE_DIR_PFCP"
-run_tests "$VOL_BASE_DIR_PFCP" "with-pfcp/HNBGW_Tests.cfg" "osmo-stp.cfg" "with-pfcp/osmo-hnbgw.cfg"
+run_tests "$VOL_BASE_DIR_PFCP" "with-pfcp/HNBGW_Tests.cfg" $osmo_stp_cfg $osmo_hnbgw_with_pfcp_cfg
