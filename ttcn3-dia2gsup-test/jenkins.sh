8692b156da826767010b7669b527ec101cc9fd70
Pau Espin Pedrol <pespin@sysmocom.de>
2023-08-30 15:51:30 +0200

    ttcn3-dia2gsup: Avoid retrieving unexisting talloc report
    The OSMO_SUT_{HOST,PORT} envvars are used to trigger retrieving of
    talloc reportes from the VTY port in osmo-ttcn3-hacks.git
    ttcn3-tcpdump-stop.sh.
    osmo_dia2gsup is an erland app and hence it makes no sense trying to
    retrieve such information.
    
    Change-Id: I6ee3fa83758e09898d1cd05bf15bc3428e2ffd11

--- ttcn3-dia2gsup-test/jenkins.sh
+++ ttcn3-dia2gsup-test/jenkins.sh
@@ -33,8 +33,6 @@ docker run	--rm \
 		$(docker_network_params $SUBNET 103) \
 		--ulimit core=-1 \
 		-e "TTCN3_PCAP_PATH=/data" \
-		-e "OSMO_SUT_HOST=172.18.$SUBNET.20" \
-		-e "OSMO_SUT_PORT=4258" \
 		-v $VOL_BASE_DIR/dia2gsup-tester:/data \
 		--name ${BUILD_TAG}-ttcn3-dia2gsup-test \
 		$DOCKER_ARGS \
