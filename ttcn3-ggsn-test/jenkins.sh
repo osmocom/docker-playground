d8c51cce641aa7f00f9bc580d2250a2e6cdd8ede
Oliver Smith <osmith@sysmocom.de>
2024-03-21 12:35:41 +0100

    ttcn3-ggsn-test: run with multiple configurations
    The kernel ttcn3-ggsn-tests have been failing since the osmo-ggsn
    config for kernel-gtp has been adjusted for v4v6 instead of v4 only.
    
    This new configuration caused a regression with:
    * linux kernel without Pablo's GTP IPv6 patches
    * latest stable release of osmo-ggsn, which does not support IPv6
    
    As the kernel patches aren't merged yet, this caused all tests to fail.
    
    Avoid this by running the ttcn3-ggsn-test with multiple configurations:
    * v4_only: one APN with v4
    * v6_only: one APN with v6
    * v4v6_only: one APN with v4v6
    * all: multiple APNs with all of the above
    
    With this change, the tests running in the v4_only configuration will
    pass again with the stable versions of osmo-ggsn, libgtpnl and kernel.
    And with new versions, most test in the other configurations will pass
    too.
    
    As discussed with Harald, this does not only split up the configuration
    for osmo-ggsn with kernel gtp-u, but also with the userspace
    implementation. The "all" config currently only works with the
    userspace gtp implementation (OS#6106).
    
    Related: OS#6096
    Depends: osmo-ttcn3-hacks I6d94a8b18200fbb2119406827b74b83e912e3ecc
    Fixes: e24442f9 ("ttcn3-ggsn-test: kernel-gtp: add ipv6")
    Change-Id: Ia2fe0c3ed4ccf06e72fd258d085e4a79cecd5f26

--- ttcn3-ggsn-test/jenkins.sh
+++ ttcn3-ggsn-test/jenkins.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
-# Environment variables: see "Kernel test" section in README.md
+# Environment variables: see README.md
+# Possible test configurations:
+# * v4_only: one APN with v4
+# * v6_only: one APN with v6
+# * v4v6_only: one APN with v4v6
+# * all: multiple APNs with all of the above
+TEST_CONFIGS_ALL="all v4_only v6_only v4v6_only"
+TEST_CONFIGS="${TEST_CONFIGS:-$TEST_CONFIGS_ALL}"
 . ../jenkins-common.sh
 
 KERNEL_TEST="${KERNEL_TEST:-0}"
@@ -11,20 +18,41 @@ docker_images_require \
 set_clean_up_trap
 set -e
 
+clean_up() {
+	local i
+	local log
+
+	# Add a suffix to the class names in the junit-xml log
+	for i in $TEST_CONFIGS; do
+		if [ "$i" = "all" ]; then
+			continue
+		fi
+
+		for log in "$VOL_BASE_DIR"/ggsn-tester-"$i"/junit-xml-*.log; do
+			if ! [ -e "$log" ]; then
+				continue
+			fi
+			sed -i "s/classname='\([^']\+\)'/classname='\1_$i'/g" "$log"
+		done
+	done
+}
+
 # Start container with ggsn in background
 start_ggsn() {
+	local test_config="$1"
+
 	GGSN_CMD="osmo-ggsn -c /data/osmo-ggsn.cfg"
 	GGSN_DOCKER_ARGS=""
 	if [ "$KERNEL_TEST" = "1" ]; then
-		cp osmo-ggsn-kernel/initrd-ggsn.sh $VOL_BASE_DIR/ggsn/
+		cp osmo-ggsn-kernel/initrd-ggsn.sh $VOL_BASE_DIR/ggsn-"$test_config"/
 		network_replace_subnet_in_configs
 
 		kernel_test_prepare \
 			"defconfig" \
 			"osmo-ggsn-kernel/fragment.config" \
-			"$VOL_BASE_DIR/ggsn/initrd-ggsn.sh" \
+			"$VOL_BASE_DIR/ggsn-$test_config/initrd-ggsn.sh" \
 			"$REPO_USER/osmo-ggsn-$IMAGE_SUFFIX" \
-			-v $VOL_BASE_DIR/ggsn:/data
+			-v $VOL_BASE_DIR/ggsn-"$test_config":/data
 
 		GGSN_CMD="/kernel-test/run-qemu.sh"
 		GGSN_DOCKER_ARGS="
@@ -46,23 +74,25 @@ start_ggsn() {
 			--device /dev/net/tun:/dev/net/tun \
 			--sysctl net.ipv6.conf.all.disable_ipv6=0 \
 			--ulimit core=-1 \
-			-v $VOL_BASE_DIR/ggsn:/data \
+			-v $VOL_BASE_DIR/ggsn-"$test_config":/data \
 			--name ${BUILD_TAG}-ggsn -d \
 			$DOCKER_ARGS \
 			$GGSN_DOCKER_ARGS \
 			$REPO_USER/osmo-ggsn-$IMAGE_SUFFIX \
 			/bin/sh -c "$GGSN_CMD >/data/osmo-ggsn.log 2>&1"
 
-	kernel_test_wait_for_vm "$VOL_BASE_DIR/ggsn/osmo-ggsn.log"
+	kernel_test_wait_for_vm "$VOL_BASE_DIR/ggsn-$test_config/osmo-ggsn.log"
 }
 
 # Start docker container with testsuite in foreground
 start_testsuite() {
+	local test_config="$1"
+
 	docker run	--rm \
 			--sysctl net.ipv6.conf.all.disable_ipv6=0 \
 			$(docker_network_params $SUBNET 202) \
 			--ulimit core=-1 \
-			-v $VOL_BASE_DIR/ggsn-tester:/data \
+			-v $VOL_BASE_DIR/ggsn-tester-"$test_config":/data \
 			-e "TTCN3_PCAP_PATH=/data" \
 			-e "OSMO_SUT_HOST=$OSMO_SUT_HOST" \
 			-e "OSMO_SUT_PORT=4260" \
@@ -70,12 +100,6 @@ start_testsuite() {
 			$DOCKER_ARGS \
 			$REPO_USER/ttcn3-ggsn-test
 }
-
-mkdir $VOL_BASE_DIR/ggsn-tester
-cp ttcn3/GGSN_Tests.cfg $VOL_BASE_DIR/ggsn-tester/
-write_mp_osmo_repo "$VOL_BASE_DIR/ggsn-tester/GGSN_Tests.cfg"
-
-mkdir $VOL_BASE_DIR/ggsn
 
 network_create
 
@@ -85,8 +109,28 @@ else
 	CONFIGS_DIR="osmo-ggsn"
 fi
 
-cp "$CONFIGS_DIR"/osmo-ggsn.cfg $VOL_BASE_DIR/ggsn/osmo-ggsn.cfg
-network_replace_subnet_in_configs
+for i in $TEST_CONFIGS_ALL; do
+	if ! test_config_enabled "$i"; then
+		continue
+	fi
 
-start_ggsn
-start_testsuite
+	# OS#6106: Currently it is not possible to configure multiple APNs with
+	# gtpu-mode kernel-gtp in OsmoGGSN, because it cannot share the GTP-U
+	# bind socket between tunnels.
+	if [ "$i" = "all" ] && [ "$KERNEL_TEST" = "1" ]; then
+		continue
+	fi
+
+	mkdir $VOL_BASE_DIR/ggsn-tester-"$i"
+	cp ttcn3/"$i"/GGSN_Tests.cfg $VOL_BASE_DIR/ggsn-tester-"$i"/GGSN_Tests.cfg
+	write_mp_osmo_repo "$VOL_BASE_DIR/ggsn-tester-$i/GGSN_Tests.cfg"
+
+	mkdir $VOL_BASE_DIR/ggsn-"$i"
+	cp "$CONFIGS_DIR"/"$i"/osmo-ggsn.cfg "$VOL_BASE_DIR"/ggsn-"$i"/osmo-ggsn.cfg
+	network_replace_subnet_in_configs
+
+	start_ggsn "$i"
+	start_testsuite "$i"
+
+	docker_kill_wait "$BUILD_TAG"-ggsn
+done
