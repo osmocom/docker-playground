882c26b0d81586f8e372961989869ae6dfae857c
Pau Espin Pedrol <pespin@sysmocom.de>
2024-03-28 21:24:35 +0100

    Introduce ttcn3-asterisk-ims-ue-test
    This dockerized setup allows running the Asterisk_Tests ttcn3 testsuite
    against Asterisk from the asterisk-master docker image, in order to test
    the IMS UE feature which will be developed soon.
    
    Related: SYS#6782
    Depends: osmo-ttcn3-hacks.git Change-Id I66f776d5df6fb5dc488d9e589b84a6b2385406e8
    Change-Id: Idd262b9224ea879a6e941792944fe0b0db3e68ba

---
+++ ttcn3-asterisk-ims-ue-test/jenkins.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+. ../jenkins-common.sh
+IMAGE_SUFFIX="${IMAGE_SUFFIX:-master}"
+docker_images_require \
+	"asterisk-$IMAGE_SUFFIX" \
+	"ttcn3-asterisk-ims-ue-test"
+
+set_clean_up_trap
+set -e
+
+mkdir $VOL_BASE_DIR/asterisk-ims-ue-tester
+mkdir $VOL_BASE_DIR/asterisk-ims-ue-tester/unix
+cp Asterisk_Tests.cfg $VOL_BASE_DIR/asterisk-ims-ue-tester/
+write_mp_osmo_repo "$VOL_BASE_DIR/asterisk-ims-ue-tester/Asterisk_Tests.cfg"
+
+mkdir $VOL_BASE_DIR/asterisk
+cp asterisk/* $VOL_BASE_DIR/asterisk/
+
+network_create
+network_replace_subnet_in_configs
+
+echo Starting container with Asterisk
+docker run	--rm \
+		$(docker_network_params $SUBNET 10) \
+		--ulimit core=-1 \
+		-v $VOL_BASE_DIR/asterisk:/data \
+		--name ${BUILD_TAG}-asterisk -d \
+		$DOCKER_ARGS \
+		$REPO_USER/asterisk-$IMAGE_SUFFIX \
+		/bin/sh -c "/data/asterisk.sh >/data/asterisk.console.log 2>&1"
+
+# Leave some time for asterisk to start:
+sleep 3
+
+echo Starting container with Asterisk testsuite
+docker run	--rm \
+		$(docker_network_params $SUBNET 103) \
+		--ulimit core=-1 \
+		-e "TTCN3_PCAP_PATH=/data" \
+		-v $VOL_BASE_DIR/asterisk-ims-ue-tester:/data \
+		--name ${BUILD_TAG}-ttcn3-asterisk-ims-ue-test \
+		$DOCKER_ARGS \
+		$REPO_USER/ttcn3-asterisk-ims-ue-test
