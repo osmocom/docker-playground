9ca9bed0acf4fbf82fa56ada8be7f62cbbc9190d
Vadim Yanitskiy <vyanitskiy@sysmocom.de>
2024-06-22 03:50:45 +0700

    ttcn3-s1gw-test: fix using unassigned addr for osmo-s1gw
    The S1GW container gets assigned the primary addr 172.18.10.100/24
    by docker_network_params().  The secondary addr 172.18.10.200/24,
    which is used for eNB connections, needs to be assigned manually.
    
    This patch finally makes ttcn3-s1gw-test pass.
    
    Change-Id: I109a5feaca5acf050008e883cc8b4e1e28beebab
    Related: SYS#6772

--- ttcn3-s1gw-test/jenkins.sh
+++ ttcn3-s1gw-test/jenkins.sh
@@ -14,6 +14,7 @@ cp S1GW_Tests.cfg $VOL_BASE_DIR/s1gw-tester/
 write_mp_osmo_repo "$VOL_BASE_DIR/s1gw-tester/S1GW_Tests.cfg"
 
 mkdir $VOL_BASE_DIR/s1gw
+cp s1gw.sh $VOL_BASE_DIR/s1gw/
 cp osmo-s1gw.config $VOL_BASE_DIR/s1gw/
 
 network_create
@@ -23,10 +24,13 @@ echo "Starting container with osmo-s1gw"
 docker run	--rm \
 		$(docker_network_params $SUBNET 100) \
 		--ulimit core=-1 \
+		--cap-add=NET_ADMIN \
+		-e "ERL_FLAGS=-config /data/osmo-s1gw.config" \
 		-v $VOL_BASE_DIR/s1gw:/data \
 		--name ${BUILD_TAG}-s1gw -d \
 		$DOCKER_ARGS \
-		$REPO_USER/osmo-s1gw-$IMAGE_SUFFIX
+		$REPO_USER/osmo-s1gw-$IMAGE_SUFFIX \
+		/bin/sh -c "/data/s1gw.sh > /data/osmo-s1gw.log 2>&1"
 
 # Give some time to osmo-s1gw to be fully started; it's a bit slow...
 sleep 2
