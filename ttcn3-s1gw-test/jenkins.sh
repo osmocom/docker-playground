osmocom/docker-playground

commit 9ca9bed0acf4fbf82fa56ada8be7f62cbbc9190d

ttcn3-s1gw-test: fix using unassigned addr for osmo-s1gw
The S1GW container gets assigned the primary addr 172.18.10.100/24
by docker_network_params().  The secondary addr 172.18.10.200/24,
which is used for eNB connections, needs to be assigned manually.

This patch finally makes ttcn3-s1gw-test pass.

Change-Id: I109a5feaca5acf050008e883cc8b4e1e28beebab
Related: SYS#6772

ttcn3-s1gw-test/jenkins.sh
@@ -14,6 +14,7 @@ cp S1GW_Tests.cfg $VOL_BASE_DIR/s1gw-tester/
 write_mp_osmo_repo "$VOL_BASE_DIR/s1gw-tester/S1GW_Tests.cfg"
 
 mkdir $VOL_BASE_DIR/s1gw
+cp s1gw.sh $VOL_BASE_DIR/s1gw/
 cp osmo-s1gw.config $VOL_BASE_DIR/s1gw/
 
 network_create
@@ -23,10 +24,13 @@ echo "Starting container with osmo-s1gw"
 docker run	--rm \
 		$(docker_network_params $SUBNET 100) \
 		--ulimit core=-1 \
+		--cap-add=NET_ADMIN \
+		-e "ERL_FLAGS=-config /data/osmo-s1gw.config" \
 		-v $VOL_BASE_DIR/s1gw:/data \
 		--name ${BUILD_TAG}-s1gw -d \
 		$DOCKER_ARGS \
-		$REPO_USER/osmo-s1gw-$IMAGE_SUFFIX
+		$REPO_USER/osmo-s1gw-$IMAGE_SUFFIX \
+		/bin/sh -c "/data/s1gw.sh > /data/osmo-s1gw.log 2>&1"
 
 # Give some time to osmo-s1gw to be fully started; it's a bit slow...
 sleep 2

ttcn3-s1gw-test/s1gw.sh
@@ -1,0 +1,8 @@
+#!/bin/sh -ex
+
+# the subnet prefix is to be modified by network_replace_subnet_in_configs()
+ip addr add 172.18.10.100/24 dev eth0 || true  # primary address already set by docker_network_params()
+ip addr add 172.18.10.200/24 dev eth0  # secondary address for eNB -> S1GW connections
+
+# drop the root privileges and finally start osmo-s1gw
+su build -c "/tmp/osmo-s1gw/_build/default/bin/osmo-s1gw"

commit 7be93fc721de12b6ff0b47fd346848e5107c68ee

ttcn3-s1gw-test: start osmo-s1gw container with --user=root
For some reason, on Arch with docker 26.1.3 it implicitly starts
with --user=root, while with docker 20.10.5 on Debian bookworm it
starts with --user=build.  This results in permission errors:

  + ip addr add 172.18.74.100/24 dev eth0
  RTNETLINK answers: Operation not permitted
  + true
  + ip addr add 172.18.74.200/24 dev eth0
  RTNETLINK answers: Operation not permitted

Pass --user=root explicitly to bring consistency and fix those.
Hopefully, ttcn3-s1gw-test will be passing on Jenkins with this patch.

Change-Id: I5cc67d1c3192fd55560c16c8cc44b13b2c9b3e98
Fixes: 9ca9bed "ttcn3-s1gw-test: fix using unassigned addr for osmo-s1gw"

ttcn3-s1gw-test/jenkins.sh
@@ -23,6 +23,7 @@ network_replace_subnet_in_configs
 echo "Starting container with osmo-s1gw"
 docker run	--rm \
 		$(docker_network_params $SUBNET 100) \
+		--user=root \
 		--ulimit core=-1 \
 		--cap-add=NET_ADMIN \
 		-e "ERL_FLAGS=-config /data/osmo-s1gw.config" \