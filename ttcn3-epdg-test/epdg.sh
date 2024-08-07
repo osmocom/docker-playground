00e682457ab1c6ffbaa9f6a566073514d0ec11db
Oliver Smith <osmith@sysmocom.de>
2024-08-07 11:37:43 +0200

    ttcn3-epdg-test: remove latest code path
    Use the same config in latest as in master, as a new version 0.1.0 has
    been released recently. This fixes the two currently failing tests on
    latest. The ttcn3-epdg-test has not been replaced by testenv (OS#6494)
    yet, so let's fix it here.
    
    Change-Id: I522824a6df3c58d26788c8d9e978759e7ec8a462

--- ttcn3-epdg-test/epdg.sh
+++ ttcn3-epdg-test/epdg.sh
@@ -11,11 +11,7 @@ ip link add $UE_IFACE type dummy
 ip addr add $UE_ADDR dev $UE_IFACE
 ip link set $UE_IFACE up
 ip rule add from $UE_SUBNET table 45
-if [ "$IMAGE_SUFFIX" = "latest" ]; then
-	ERL_FLAGS='-config /data/osmo-epdg.latest.config' /usr/bin/osmo-epdg &
-else
-	ERL_FLAGS='-config /data/osmo-epdg.config' /tmp/osmo-epdg/_build/default/bin/osmo-epdg &
-fi
+ERL_FLAGS='-config /data/osmo-epdg.config' /tmp/osmo-epdg/_build/default/bin/osmo-epdg &
 MYPID=$!
 
 # We cannot set a route for the interface until it is created by osmo-epdg...
