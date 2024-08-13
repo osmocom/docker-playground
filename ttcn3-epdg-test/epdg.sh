osmocom/docker-playground

commit 00e682457ab1c6ffbaa9f6a566073514d0ec11db

ttcn3-epdg-test: remove latest code path
Use the same config in latest as in master, as a new version 0.1.0 has
been released recently. This fixes the two currently failing tests on
latest. The ttcn3-epdg-test has not been replaced by testenv (OS#6494)
yet, so let's fix it here.

Change-Id: I522824a6df3c58d26788c8d9e978759e7ec8a462

ttcn3-epdg-test/epdg.sh
@@ -11,11 +11,7 @@ ip link add $UE_IFACE type dummy
 ip addr add $UE_ADDR dev $UE_IFACE
 ip link set $UE_IFACE up
 ip rule add from $UE_SUBNET table 45
-if [ "$IMAGE_SUFFIX" = "latest" ]; then
-	ERL_FLAGS='-config /data/osmo-epdg.latest.config' /usr/bin/osmo-epdg &
-else
-	ERL_FLAGS='-config /data/osmo-epdg.config' /tmp/osmo-epdg/_build/default/bin/osmo-epdg &
-fi
+ERL_FLAGS='-config /data/osmo-epdg.config' /tmp/osmo-epdg/_build/default/bin/osmo-epdg &
 MYPID=$!
 
 # We cannot set a route for the interface until it is created by osmo-epdg...

ttcn3-epdg-test/osmo-epdg.latest.config
@@ -1,87 +1,0 @@
-[
- %% ===========================================
- %% Application config
- %% ===========================================
- {osmo_epdg,
-  [% GSUP Server connection parameters
-   {gsup_local_ip, "172.18.10.20"},
-   {gsup_local_port, 4222},
-   % Diameter Connection parameters
-   {dia_swx_remote_ip, "172.18.10.103"},
-   {dia_swx_remote_port, 3868},
-   {dia_swx_proto, sctp},
-   {dia_swx_connect_timer, 200},
-   {dia_swx_watchdog_timer, 6000},
-   {dia_swx_watchdog_config,
-    [{okay, 1},
-     {suspect, 1}]},
-   {dia_swx_vendor_id, 0},
-   {dia_swx_origin_host, "epdg.localdomain"},
-   {dia_swx_origin_realm, "localdomain"},
-   {dia_swx_context_id, "epdg@localdomain"},
-   % Diameter s6b Connection parameters
-   {dia_s6b_local_ip, "172.18.10.20"},
-   {dia_s6b_local_port, 3869},
-   {dia_s6b_proto, sctp},
-   {dia_s6b_connect_timer, 200},
-   {dia_s6b_watchdog_timer, 6000},
-   {dia_s6b_watchdog_config,
-   [{okay, 1},
-    {suspect, 1}]},
-   {dia_s6b_vendor_id, 0},
-   {dia_s6b_origin_host, "aaa.localdomain"},
-   {dia_s6b_origin_realm, "localdomain"},
-   {dia_s6b_context_id, "aaa@localdomain"},
-   % GTPv2C Connection parameters
-   {gtpc_local_ip, "172.18.10.20"},
-   {gtpc_local_port, 2123},
-   {gtpc_remote_ip, "172.18.10.103"},
-   {gtpc_remote_port, 2123}
-  ]
- },
- %% ===========================================
- %% gtp_u_kmod config
- %% ===========================================
-  {gtp_u_kmod, [
-    {sockets, [{gtp0, [%% ip: IP Address assigned at the tunnel:
-                       {ip, {172,18,10,20}},
-                       %% Allow binding to an IP address that is nonlocal or does not (yet) exist (IP_FREEBIND):
-                       freebind,
-                       % Create gtp tundev with role SGSN:
-                       {role, sgsn}
-                     ]
-              }]
-    }
-  ]},
- %% ===========================================
- %% gen_socket config
- %% ===========================================
- {gen_socket, [
-  {libdir, "/usr/lib"}
-]},
- %% ===========================================
- %% Lager logging config
- %% ===========================================
-  {lager, [
-    {log_root, "/data/"},
-    {colored, true},
-    {handlers,
-      [{lager_console_backend, [{level, debug}]},
-      {lager_file_backend,
-        [{file, "console.log"}, {level, debug}, {size, 104857600}, {date, "$D0"}, {count, 10}]},
-      {lager_file_backend,
-        [{file, "error.log"}, {level, error}, {size, 104857600}, {date, "$D0"}, {count, 10}]}]},
-    {crash_log, "crash.log"},
-    {crash_log_msg_size, 65536},
-    {crash_log_size, 104857600},
-    {crash_log_date, "$D0"},
-    {crash_log_count, 10},
-    {error_logger_redirect, true}
-  ]},
-  {kernel, [
-   {logger, [{handler, debug, logger_std_h,  % {handler, HandlerId, Module,
-              #{config => #{file => "/data/erlang.log"}}
-            }]
-    }
-  ]}
-].

commit 78b5c87737567be1add91d1d33f195b4bd0365e2

ttcn3-epdg-test: fix osmo-epdg path for latest
Restore the proper path for osmo-epdg when running with -latest, which
has been removed by accident in the earlier commit that should only
have changed the config.

Fix for:
/data/epdg.sh: line 14: /tmp/osmo-epdg/_build/default/bin/osmo-epdg: No such file or directory

Fixes: 00e68245 ("ttcn3-epdg-test: remove latest code path")
Change-Id: I0547f62a3610a62e90ad767f447c68851e8a725d

ttcn3-epdg-test/epdg.sh
@@ -11,7 +11,11 @@ ip link add $UE_IFACE type dummy
 ip addr add $UE_ADDR dev $UE_IFACE
 ip link set $UE_IFACE up
 ip rule add from $UE_SUBNET table 45
-ERL_FLAGS='-config /data/osmo-epdg.config' /tmp/osmo-epdg/_build/default/bin/osmo-epdg &
+if [ "$IMAGE_SUFFIX" = "latest" ]; then
+	ERL_FLAGS='-config /data/osmo-epdg.config' /usr/bin/osmo-epdg &
+else
+	ERL_FLAGS='-config /data/osmo-epdg.config' /tmp/osmo-epdg/_build/default/bin/osmo-epdg &
+fi
 MYPID=$!
 
 # We cannot set a route for the interface until it is created by osmo-epdg...