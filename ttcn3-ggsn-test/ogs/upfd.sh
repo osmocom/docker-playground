b215006476583d31a1a10d60bc2c4a1b231e2621
Oliver Smith <osmith@sysmocom.de>
2024-10-25 14:01:08 +0200

    Remove ttcn3 testsuites ported to testenv, part 2
    Related: OS#6494
    Change-Id: I61fd5523dbd5664763b006d5ba9a368cd9caf9b9

--- ttcn3-ggsn-test/ogs/upfd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-set -x
-/data/upfd-setup.sh
-upfd_bin="$(command -v open5gs-upfd)"
-# so_bindtodevice cfg requires CAP_NET_RAW:
-setcap cap_net_raw+ep "$upfd_bin"
-su - osmocom -c "$upfd_bin $*"
