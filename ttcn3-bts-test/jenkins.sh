osmocom/docker-playground

commit 8540ae7e1b2f1e6328d0ee850259f3b83b12ec4c

ttcn3-bts-test: use an older version of fake_trx.py
The latest version of fake_trx.py [re-]introduces burst queueing [1],
making its behavior more closely resemble that of a real transceiver.
However, this change has also introduced stability issues, leading
to increased flakiness in ttcn3-bts-test.

As a temporary workaround, use an older version predating this change.
Unset the "--sched-rr-prio" [2], which is not supported by that version.

Change-Id: I251082474bb29d0169f88b5bd545c97d487c8353
Related: [1] osmocom-bb.git I41291708effdd2c767be680fff22ffbd9a56815e
Related: [2] 6374a9e ("ttcn3-bts-test: Run fake_trx with sched-prio-rr")
Related: OS#6672

ttcn3-bts-test/Makefile
@@ -1,3 +1,6 @@
+# XXX: use an older version of fake_trx.py, that predates
+# commit 0f4714776a9c9b64c4a7268eb8a346f304835565 (OS#6672).
+OSMO_BB_BRANCH=ttcn3-bts-test
 
 include ../make/Makefile
 

ttcn3-bts-test/jenkins.sh
@@ -74,7 +74,6 @@ start_fake_trx() {
 				--log-file-level DEBUG \
 				--log-file-time \
 				--log-level INFO \
-				--sched-rr-prio 30 \
 				-R $SUB4_PREFIX.$SUBNET.20 \
 				-r $SUB4_PREFIX.$SUBNET.22 \
 				--trx TRX1@$SUB4_PREFIX.$SUBNET.20:5700/1 \

commit 86ce525e8ba02b935d2042935c7aed3d6ca75c03

fixup: ttcn3-bts-test: use an older version of fake_trx.py
Setting OSMO_BB_BRANCH=ttcn3-bts-test did not achieve the goal, because
it was not consumed by the osmocom-bb-host-master's Makefile.  Setting
it in jenkins.sh does the trick.

Change-Id: Ib6857fafd4085aae91eec9c67f08766bdabeb2b7
Fixes: 8540ae7 ("ttcn3-bts-test: use an older version of fake_trx.py")

ttcn3-bts-test/Makefile
@@ -1,7 +1,3 @@
-# XXX: use an older version of fake_trx.py, that predates
-# commit 0f4714776a9c9b64c4a7268eb8a346f304835565 (OS#6672).
-OSMO_BB_BRANCH=ttcn3-bts-test
-
 include ../make/Makefile
 
 CFG_SRC_FILES = \

ttcn3-bts-test/jenkins.sh
@@ -2,6 +2,10 @@
 TEST_CONFIGS_ALL="generic virtphy oml hopping"
 TEST_CONFIGS="${TEST_CONFIGS:-"generic oml hopping"}"
 
+# XXX: use an older version of fake_trx.py, that predates
+# commit 0f4714776a9c9b64c4a7268eb8a346f304835565 (OS#6672).
+export OSMO_BB_BRANCH=ttcn3-bts-test
+
 . ../jenkins-common.sh
 IMAGE_SUFFIX="${IMAGE_SUFFIX:-master}"
 # NOTE: there is no osmocom-bb-host-latest, hence always use master!