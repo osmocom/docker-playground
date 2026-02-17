b38395309c787ec019f4da8202862a6db0dde655
Oliver Smith <osmith@sysmocom.de>
2026-02-17 13:12:28 +0100

    common/wrapper_core_bt_on_error: import
    Import this script from osmo-ci dfa9fcca
    ("scripts/wrapper_core_bt_on_error: new script"), so it can be used in
    ttcn3-asterisk-ims-ue-test in a follow-up patch.
    
    Change-Id: I773ff8b8c07c96e1b5b417fa28518ac7dc2e14ec

---
+++ common/wrapper_core_bt_on_error.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+# Copyright 2025 sysmocom - s.f.m.c. GmbH
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Run a program and check for coredumps if it does not exit with 0. If there
+# are any coredumps, then show the backtrace.
+msg() {
+	echo "[wrapper_core_bt_on_error] $@"
+}
+
+if [ $# -lt 1 ]; then
+	echo "usage: wrapper_core_bt_on_error.sh PROGRAM [ARG1 [ARG2 […]]]"
+	exit 1
+fi
+
+ulimit -c unlimited
+
+"$@"
+RC=$?
+
+if [ "$RC" != 0 ]; then
+	for i in $(find -name 'core' -type f); do
+		msg "Found coredump: $i"
+		execfn="$(file "$i" | grep -P -o "execfn: '.*?'" | cut -d "'" -f 2)"
+		if [ -z "$execfn" ] || ! [ -e "$execfn" ]; then
+			msg "Failed to get execfn, ignoring..."
+			continue
+		fi
+
+		echo
+		gdb --batch \
+			"$execfn" \
+			"$i" \
+			-ex bt \
+			| tee "$i.backtrace"
+		echo
+	done
+	exit $RC
+fi
